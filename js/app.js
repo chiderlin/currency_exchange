8893a5e04100280345592fb80e8d5d9d92f6cf98
Chi Der <chider@Chide-MBP.lan>
2023-07-30 22:51:29 -0700

    update js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const helment = require('helmet');
+const cors = require('cors');
 const app = express();
+
+dotenv.config();
 
 const router = require('./routes');
 
+app.use(helment());
+app.use(cors());
 app.use(router);
 
 module.exports = app;
